693100d8ad7216e21b7f8538dfbf5c17eecd26ec
geoffspeicher <geoffspeicher@b04c1738-1009-11df-95c3-6ff5f211243c>
2011-10-29 17:07:01 +0000

    Explicity mark Java sources with appropriate copyright information. This should have been done long ago and it recently came to my attention that it is only made more necessary since we have accepted contributions from the community.
    Unless otherwise indicated, these sources have always been subject to the
    Apache License, Version 2.0 as was stated on the Google Code project page
    since its inception.  According to the License terms, all contributions
    are subject to the same License unless otherwise indicated.  Most of the
    contributions did not make any indication otherwise, which implies tacit
    agreement with the License, Section 5, "Submission of Contributions."
    
      http://code.google.com/p/raphaelgwt/
      http://www.apache.org/licenses/LICENSE-2.0
    
    It is indeed a sad day when such things are brought to light, but until
    now the terms have been in effect anyway so I'd rather be explicit about
    it than sweep it under the carpet.  Hurt me with the truth and all that.

--- samples/dnd/src/com/hydro4ge/dnd/client/Demo.java
+++ samples/dnd/src/com/hydro4ge/dnd/client/Demo.java
@@ -1,15 +1,17 @@
 package com.hydro4ge.dnd.client;
 
+/*
+ * the following code was copied almost verbatim from
+ *   http://code.google.com/p/gwt-dnd/wiki/GettingStarted
+ *
+ * replacing Image with DraggableCircle, defined in this package
+ */
+
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Demo implements EntryPoint {
-
-  // the following code was copied almost verbatim from
-  //   http://code.google.com/p/gwt-dnd/wiki/GettingStarted
-  //
-  // simply replacing Image with DraggableCircle, defined in this package
 
   public void onModuleLoad() {
     // ensure the document BODY has dimensions in standards mode
