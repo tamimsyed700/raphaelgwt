1a18f921c053c8ca2d1d29b787876e273d833fc1
geoffspeicher <geoffspeicher@b04c1738-1009-11df-95c3-6ff5f211243c>
2011-10-29 17:28:35 +0000

    Change AnimationCallback from an abstract class to an interface.
    This commit breaks any classes that extend AnimationCallback;
    you will need to change your class declaration to implement it
    instead of extending it.  Aside from this simple change in the
    declaration, user code is unaffected.
    
    Note that instances of anonymous AnimationCallbacks are totally
    unaffected by this change, because the syntax is identical for
    creating new instances of anonymous classes that implement an
    interface verses those that extend a class.  Funny thing about
    Java, right there.

--- trunk/src/com/hydro4ge/raphaelgwt/client/AnimationCallback.java
+++ trunk/src/com/hydro4ge/raphaelgwt/client/AnimationCallback.java
@@ -16,10 +16,7 @@
 
 package com.hydro4ge.raphaelgwt.client;
 
-public abstract class AnimationCallback {
-  public abstract void onComplete();
-  static public void fire(AnimationCallback c) {
-	c.onComplete();
-  }
+public interface AnimationCallback {
+  public void onComplete();
 }
 
